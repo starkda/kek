bb711da6c022546d8c45685fda1d7c6c1f288a67
starkda <d.vakhidov@innopolis.universiry>
2023-09-11 14:23:23 +0300

    базовая конфигурация файла

---
+++ src/main/java/com/example/kek/KekApplication.java
@@ -0,0 +1,13 @@
+package com.example.kek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KekApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kek/KekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
