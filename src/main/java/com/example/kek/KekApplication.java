9c7047312bc41a31281125684649d64acc3b6fab
starkda <d.vakhidov@innopolis.universiry>
2023-09-21 13:21:54 +0300

    добавил парсер лекс анализатора

--- src/main/java/com/example/kek/KekApplication.java
+++ src/main/java/com/example/kek/KekApplication.java
@@ -1,13 +1,17 @@
 package com.example.kek;
 
+import com.example.kek.lexical_analyzer.Parser;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class KekApplication {
-
 	public static void main(String[] args) {
-		SpringApplication.run(KekApplication.class, args);
+		String input_file = "input.txt";
+		if (args.length > 0){
+			input_file = args[0];
+		}
+
+		System.out.println(Parser.generateTokens(input_file));
 	}
 
 }
